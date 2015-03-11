8921fcb8325cab2860c841aa734b6e81193d4669
Jeet Dhillon <jitdhillon87@gurjit-singhs-MacBook-Pro.local>
2015-03-10 20:36:46 -0700

    added comments and made it more obvious so user can scroll the details

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,17 +10,26 @@
 angular.module('facesofHistoryApp')
   .controller('MainCtrl', function ($scope, face) {
     
-    
+    /**
+     * [init intialize the application]
+     * @return {[nothing]}
+     */
     var init = function init(){
     	$scope.faces = face.faces;
     	$scope.clearPerson()
     }
-
+    /**
+     * [saveNewPerson add new person to faces API]
+     * @return {[null]}
+     */
     $scope.saveNewPerson = function saveNewPerson(){
     	face.faces.$add($scope.newPerson);
     	$scope.clearPerson();
     }
-
+    /**
+     * [clearPerson create newPerson and clear preexiting object ]
+     * @return {[null]}
+     */
     $scope.clearPerson = function clearPerson(){
     	$scope.newPerson = {
 	    	name: '',
