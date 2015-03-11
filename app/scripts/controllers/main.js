1c630982d3225382ad37d65c87672e199c88bd5f
Jeet Dhillon <jitdhillon87@gurjit-singhs-MacBook-Pro.local>
2015-03-10 19:19:30 -0700

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name facesofHistoryApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the facesofHistoryApp
+ */
+angular.module('facesofHistoryApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
